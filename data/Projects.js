00d8899cec2263c246209c291f8212449ff4719b
sbyuxD ! <irmalove999@gmail.com>
2026-08-09 21:41:49 +0700

    upload: data/Projects.js

---
+++ data/Projects.js
@@ -0,0 +1,22 @@
+export const Projects = [
+  {
+    id: 1,
+    name: 'Rest Api',
+    slug: 'sgcid',
+    img: 'sgcid',
+    demo: 'https://api.wolf.vercel.app',
+    sourceCode: 'https://api.wolf.vercel.app',
+    description:
+      'Simple Rest API',
+  },
+  {
+    id: 2,
+    name: 'Coming Soon Project',
+    slug: 'coming-soon-project',
+    img: 'coming-soon',
+    demo: '#',
+    sourceCode: '#',
+    description:
+      'Nantikan project-project mendatang lainnya.',
+  }
+];
