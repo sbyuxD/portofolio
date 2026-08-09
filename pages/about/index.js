c1b963dfb3e86c836580a0fa06752837aef9cefb
sbyuxD ! <irmalove999@gmail.com>
2026-08-09 21:41:58 +0700

    upload: pages/about/index.js

---
+++ pages/about/index.js
@@ -0,0 +1,37 @@
+import Layouts from '@/components/Layouts';
+import { Skills } from '@/data/Skills';
+import { motion } from 'framer-motion';
+import { ContentAnimation, FadeAnimation } from '@/components/Animations';
+
+const About = () => {
+  return (
+    <Layouts pageTitle=" | About">
+      <section className="relative flex w-full flex-col items-center justify-evenly py-40 md:h-screen md:flex-row md:overflow-hidden">
+        <motion.span {...FadeAnimation} className="title-page">
+          ABOUT
+        </motion.span>
+
+        <motion.div {...ContentAnimation} className="relative order-2 flex w-3/4 flex-col items-end justify-end md:order-1 md:w-1/3">
+          <div className="my-5 mr-2 text-left font-semibold text-primary-light">My Skills</div>
+          <div className="flex w-4/5 flex-wrap justify-evenly">
+            {Skills.map((skill) => (
+              <div key={skill.id} className="skills-icon">
+                <svg className="p-2" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d={skill.svg} />
+                </svg>
+              </div>
+            ))}
+          </div>
+        </motion.div>
+
+        <motion.div {...ContentAnimation} className="relative order-1 my-10 w-3/4 text-justify md:order-2 md:my-0 md:w-1/4">
+          <p className="text-md font-light text-primary-light">
+            {`Hello! My name is sbyuxD, you can call me bayuu, or whatever. I’m a Backend Developer from Kudus, Central Java, Indonesia I usually handle backend things, like APIs, databases, and server logic. I code stuff from scratch when needed, make sure everything works, scales well, and doesn’t break for no reason. I prefer simple solutions, clean code, and systems that just work without too much drama.`}
+          </p>
+        </motion.div>
+      </section>
+    </Layouts>
+  );
+};
+
+export default About;
